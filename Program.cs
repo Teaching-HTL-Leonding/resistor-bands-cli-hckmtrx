2a53ef572c6f79de3a21045ed7ff74696daa2be7
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2023-03-16 14:28:03 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 Console.OutputEncoding = System.Text.Encoding.Default;
 
 #region Constants
-const int BLACK = 0;
-const int BROWN = 1;
-const int RED = 2;
-const int ORANGE = 3;
-const int YELLOW = 4;
-const int GREEN = 5;
-const int BLUE = 6;
-const int VIOLET = 7;
-const int GRAY = 8;
-const int WHITE = 9;
-const int GOLD = 10;
-const int SILVER = 11;
+const string BLACK = "Bla";
+const string BROWN = "Bro";
+const string RED = "Red";
+const string ORANGE = "Ora";
+const string YELLOW = "Yel";
+const string GREEN = "Gre";
+const string BLUE = "Blu";
+const string VIOLET = "Vio";
+const string GRAY = "Gra";
+const string WHITE = "Whi";
+const string GOLD = "Gol";
+const string SILVER = "Sil";
 
-const string VALUE = "value";
+const string DIGIT = "digit";
 const string MULTIPLIER = "multiplier";
 const string TOLERANCE = "tolerance";
 #endregion
@@ -22,7 +22,11 @@ const string TOLERANCE = "tolerance";
 #region Main Program
 {
     string[] colorBands = args[0].Split('-');
-    if (!(colorBands.Length is 4 or 5)) { Console.WriteLine("Invalid input length. Please provide 4 or 5 color bands."); return; }
+    if (!(colorBands.Length is 4 or 5))
+    {
+        Console.WriteLine("Invalid input length. Please provide 4 or 5 color bands.");
+        return;
+    }
 
     double resistance = 0, tolerance = 0;
     for (int i = 0; i < colorBands.Length; i++)
@@ -30,16 +34,10 @@ const string TOLERANCE = "tolerance";
         try
         {
             if (((i is 0 or 1) || (colorBands.Length == 5 && i == 2))
-                && TryGetFromColorBand(colorBands[i], VALUE, out double resistanceValue))
+                && TryGetFromColorBand(colorBands[i], DIGIT, out double digitValue))
             {
                 resistance *= 10;
-                resistance += resistanceValue;
-            }
-
-            else if (i == colorBands.Length - 1
-                    && TryGetFromColorBand(colorBands[i], TOLERANCE, out double toleranceValue))
-            {
-                tolerance = toleranceValue;
+                resistance += digitValue;
             }
 
             else if (i == colorBands.Length - 2
@@ -48,122 +46,74 @@ const string TOLERANCE = "tolerance";
                 resistance *= multiplierValue;
             }
 
+            else if (i == colorBands.Length - 1
+                    && TryGetFromColorBand(colorBands[i], TOLERANCE, out double toleranceValue))
+            {
+                tolerance = toleranceValue;
+            }
+
             else { throw new ArgumentException(colorBands[i].Length == 3 ? "Invalid color code. Please use valid color codes." : "Invalid input format. Please use a hyphen (-) to separate color codes."); }
         }
         catch (ArgumentException e) { Console.WriteLine(e.Message); return; }
     }
 
     Console.WriteLine($"Resistance: {Math.Round(resistance, 2).ToString("N2")} Ω");
-    Console.WriteLine($"Tolerance: ± {tolerance} %");
+    Console.WriteLine($"Tolerance: ± {tolerance}%");
 }
 #endregion
 
 #region Methods
-double GetMultiplier(int color) => Math.Pow(10, color);
-
 bool TryGetFromColorBand(string color, string item, out double value)
 {
-    var resistors = new Dictionary<string, double?>[]
-    {
-        new Dictionary<string, double?>
-        {
-            {VALUE, BLACK},
-            {MULTIPLIER, GetMultiplier(BLACK)},
-            {TOLERANCE, null}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, BROWN},
-            {MULTIPLIER, GetMultiplier(BROWN)},
-            {TOLERANCE, 1.0}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, RED},
-            {MULTIPLIER, GetMultiplier(RED)},
-            {TOLERANCE, 2.0}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, ORANGE},
-            {MULTIPLIER, GetMultiplier(ORANGE)},
-            {TOLERANCE, null}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, YELLOW},
-            {MULTIPLIER, GetMultiplier(YELLOW)},
-            {TOLERANCE, null}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, GREEN},
-            {MULTIPLIER, GetMultiplier(GREEN)},
-            {TOLERANCE, 0.5}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, BLUE},
-            {MULTIPLIER, GetMultiplier(BLUE)},
-            {TOLERANCE, 0.25}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, VIOLET},
-            {MULTIPLIER, GetMultiplier(VIOLET)},
-            {TOLERANCE, 0.1}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, GRAY},
-            {MULTIPLIER, GetMultiplier(GRAY)},
-            {TOLERANCE, 0.05}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, WHITE},
-            {MULTIPLIER, GetMultiplier(WHITE)},
-            {TOLERANCE, null}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, null},
-            {MULTIPLIER, 0.1},
-            {TOLERANCE, 5.0}
-        },
-        new Dictionary<string, double?>
-        {
-            {VALUE, null},
-            {MULTIPLIER, 0.01},
-            {TOLERANCE, 10.0}
-        }
-    };
-
-    int arrayItem = color switch
-    {
-        "Bla" => BLACK,
-        "Bro" => BROWN,
-        "Red" => RED,
-        "Ora" => ORANGE,
-        "Yel" => YELLOW,
-        "Gre" => GREEN,
-        "Blu" => BLUE,
-        "Vio" => VIOLET,
-        "Gra" => GRAY,
-        "Whi" => WHITE,
-        "Gol" => GOLD,
-        "Sil" => SILVER,
-        _ => -1
-    };
-
     value = 0;
-    if (arrayItem == -1) { return false; }
+    string[] colors = { BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GRAY, WHITE };
+
+    int arrayIndex = Array.IndexOf(colors, color);
+
+    if (arrayIndex == -1)
+    {
+        if (color is GOLD or SILVER && item != DIGIT)
+        {
+            value = color switch
+            {
+                GOLD => item == MULTIPLIER ? 0.1 : 5,
+                SILVER => item == MULTIPLIER ? 0.01 : 10,
+                _ => value
+            };
+
+            return true;
+        }
+
+        return false;
+    }
     else
     {
-        double? output = resistors[arrayItem][item];
+        if (item == TOLERANCE)
+        {
+            if (color is BLACK or ORANGE or YELLOW or WHITE) { return false; }
 
-        if (output == null) { return false; }
-        else { value = (double)output; return true; }
+            value = color switch
+            {
+                BROWN => 1,
+                RED => 2,
+                GREEN => 0.5,
+                BLUE => 0.25,
+                VIOLET => 0.1,
+                GRAY => 0.05,
+                _ => value
+            };
+        }
+        else
+        {
+            value = item switch
+            {
+                DIGIT => arrayIndex,
+                MULTIPLIER => Math.Pow(10, arrayIndex),
+                _ => value
+            };
+        }
+
+        return true;
     }
 }
 #endregion
